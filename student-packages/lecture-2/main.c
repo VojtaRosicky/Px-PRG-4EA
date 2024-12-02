2980557c2ecccfb1e7eea41331ed1463855a9f68
VojtaRosicky <rosicvo21@sps-prosek.cz>
2024-12-02 16:00:52 +0100

    02.12.2024

--- student-packages/lecture-2/main.c
+++ student-packages/lecture-2/main.c
@@ -1,13 +1,16 @@
 // Pico SDK libraries
 #include "pico/stdlib.h"
-#include "pico/time.h"cma
+#include "pico/time.h"
 
 // Standard libraries
 #include <stdio.h>
 #include <stdbool.h>
 
 #define LED_PIN 6
-#define BUTTON_PIN 20
+#define BUTTON_PIN_1 19
+#define BUTTON_PIN_2 20
+
+uint32_t counter = 0;
 
 void handler(uint gpio, uint32_t events);
 uint32_t millis();
@@ -26,18 +29,25 @@ int main()
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    gpio_init(BUTTON_PIN);
-    gpio_pull_up(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_init(BUTTON_PIN_1);
+    gpio_pull_up(BUTTON_PIN_1);
+    gpio_set_dir(BUTTON_PIN_1, GPIO_IN);
+
+    gpio_init(BUTTON_PIN_2);
+    gpio_pull_up(BUTTON_PIN_2);
+    gpio_set_dir(BUTTON_PIN_2, GPIO_IN);
 
     // Enable interrupts for button pin
-    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_RISE, true, &handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN_1, GPIO_IRQ_EDGE_RISE, true, &handler);
+    gpio_set_irq_enabled_with_callback(BUTTON_PIN_2, GPIO_IRQ_EDGE_RISE, true, &handler);
 
     // Infinite loop
     while (1)
     {
-        printf("Run...\n");
-        sleep_ms(1000);
+        printf("\rRun...\n");
+        printf("Hodnota counter:%d\n", counter);
+        sleep_ms(500);
+    
     }
     return 0;
 }
@@ -46,6 +56,11 @@ void handler(uint gpio, uint32_t events)
 {
     if ((millis() - lastIrgTime) >= 200) {
         gpio_put(LED_PIN, !gpio_get(LED_PIN));
+        if(gpio == BUTTON_PIN_1){
+            counter++;
+        }else if(gpio == BUTTON_PIN_2){
+            counter--;
+        }
         lastIrgTime = millis();
     }
 }
--- student-packages/lecture-3/main.c
+++ student-packages/lecture-3/main.c
@@ -4,13 +4,39 @@
 // Standard libraries
 #include <stdio.h>
 
+// More libraries
+#include "hardware/pwm.h"
+#include "hardware/gpio.h"
+
+#define LED_PIN 6
+
 int main()
 {
-    // Initialize all standard I/O
     stdio_init_all();
-    // Wait for serial monitor to reconnect
     sleep_ms(2000);
     printf("Starting...\n");
+
+    gpio_set_function(LED_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(LED_PIN);
+    pwm_config config = pwm_get_default_config();
+    pwm_config_set_clkdiv(&config, 125);
+    pwm_config_set_wrap(&config, 1000);
+    pwm_init(slice_num, &config, true);
+    pwm_set_gpio_level(LED_PIN, 1000);
+
+    while(1){
+        for (int level = 0; level <= 1000; level++) {
+            pwm_set_gpio_level(LED_PIN, level);
+            printf("\nFade Up: %d", level);
+            sleep_ms(1);
+        }
+
+        for (int level = 1000; level >= 0; level--) {
+            pwm_set_gpio_level(LED_PIN, level);
+            printf("\nFade Down: %d", level);
+            sleep_ms(1);
+            }
+    }
 
     return 0;
 }
